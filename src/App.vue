884d9b56a2711c4f4626eefb8bd7376c4134f1ed
Arne De Smedt <arnedesmdt@gmail.com>
2019-04-09 17:04:59 +0200

    add corners to popover

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,190 @@
 <template>
     <div id="app">
-        <vue-ads-popover vertical-position="bottom-top" toggle-type="click" class="vue-ads-absolute" style="left: 100px; top: 100px;">
-            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange vue-ads-text-white vue-ads-p-2">Button</div>
+        <vue-ads-popover
+            horizontal-position="middle-middle"
+            vertical-position="top-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 50px; top: 50px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="middle-middle"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 150px; top: 50px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="left-right"
+            vertical-position="middle-middle"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 250px; top: 50px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="right-left"
+            vertical-position="middle-middle"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 350px; top: 50px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+
+
+
+        <vue-ads-popover
+            horizontal-position="left-middle"
+            vertical-position="bottom-middle"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 50px; top: 150px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="left-right"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 150px; top: 150px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="left-left"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 250px; top: 150px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="right-left"
+            vertical-position="bottom-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 350px; top: 150px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+
+
+
+
+        <vue-ads-popover
+            horizontal-position="left-middle"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 50px; top: 250px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="left-right"
+            vertical-position="middle-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 150px; top: 250px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="right-left"
+            vertical-position="top-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 250px; top: 250px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="middle-left"
+            vertical-position="top-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 350px; top: 250px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+
+
+
+
+        <vue-ads-popover
+            horizontal-position="left-right"
+            vertical-position="top-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 50px; top: 350px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="left-right"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 150px; top: 350px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="right-left"
+            vertical-position="top-bottom"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 250px; top: 350px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
+        </vue-ads-popover>
+
+        <vue-ads-popover
+            horizontal-position="right-left"
+            vertical-position="bottom-top"
+            class="vue-ads-absolute"
+            arrow-color="#a0aec0"
+            style="left: 350px; top: 350px;"
+        >
+            <div slot="context" class="vue-ads-rounded-sm vue-ads-bg-gray-500 vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow">Info</div>
+            <div slot="item" class="vue-ads-w-12 vue-ads-h-12 vue-ads-bg-orange-700 vue-ads-text-white vue-ads-p-2">Button</div>
         </vue-ads-popover>
     </div>
 </template>
--- src/components/Popover.vue
+++ src/components/Popover.vue
@@ -6,16 +6,26 @@
         :vertical-position="verticalPosition"
         :horizontal-position="horizontalPosition"
     >
-        <div slot="context" :class="popoverClasses">
+        <div slot="context" :class="contextContainerClasses">
             <div
-                class="vue-ads-triangle"
-                :class="triangleClasses"
+                v-if="!disableArrow"
+                :class="arrowContainerClasses"
+                :style="arrowContainerStyles"
             >
+                <span></span>
+                <div
+                    class="vue-ads-arrow"
+                    :class="allArrowClasses"
+                    :style="arrowStyles"
+                >
+                </div>
             </div>
             <div
-                class="vue-ads-rounded-sm vue-ads-bg-grey-dark vue-ads-text-white vue-ads-p-1 vue-ads-text-sm vue-ads-shadow"
+                class="vue-ads-context"
+                :class="contextClasses"
+                :style="contextStyles"
             >
-                <slot name="info">Info</slot>
+                <slot name="context"></slot>
             </div>
         </div>
         <template slot="item">
@@ -28,6 +38,7 @@
 
 import '../../node_modules/vue-ads-context/dist/vue-ads-context.css';
 import VueAdsContext from 'vue-ads-context';
+import tailwindDefaults from '../../node_modules/tailwindcss/stubs/defaultConfig.stub';
 
 export default {
     name: 'VueAdsPopover',
@@ -68,63 +79,186 @@ export default {
             type: String,
             default: 'middle-middle',
         },
+
+        arrowSize: {
+            type: Number,
+            default: 4,
+        },
+
+        arrowColor: {
+            type: String,
+            default: '#000',
+        },
+
+        arrowClasses: {
+            type: Object,
+            default: () => {
+                return {};
+            },
+        },
+
+        hideArrow: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    data () {
+        return {
+            direction: {
+                top: 'col',
+                bottom: 'col-reverse',
+                left: 'row',
+                right: 'row-reverse',
+            },
+            opposit: {
+                top: 'bottom',
+                bottom: 'top',
+                left: 'right',
+                right: 'left',
+            },
+        };
     },
 
     computed: {
-        triangleClasses () {
+        borderWidth () {
+            return parseInt(tailwindDefaults.theme.borderWidth[this.arrowSize === 1 ? 'default' : this.arrowSize], 10);
+        },
+
+        arrow () {
+            let vertical = null;
+            let horizontal = null;
+            let container = null;
+
+            let beneathContext = this.verticalPosition.startsWith('bottom') && this.verticalPosition.endsWith('top');
+            let aboveContext = this.verticalPosition.startsWith('top') && this.verticalPosition.endsWith('bottom');
+            let rightSideOfContext = this.horizontalPosition.startsWith('right') && this.horizontalPosition.endsWith('left');
+            let leftSideOfContext = this.horizontalPosition.startsWith('left') && this.horizontalPosition.endsWith('right');
+
+            if (beneathContext) {
+                vertical = 'top';
+            } else if (aboveContext) {
+                vertical = 'bottom';
+            }
+
+            if (rightSideOfContext) {
+                horizontal = 'left';
+            } else if (leftSideOfContext) {
+                horizontal = 'right';
+            }
+
+            if (vertical) {
+                if (leftSideOfContext) {
+                    container = 'right';
+                } else if (rightSideOfContext) {
+                    container = 'left';
+                } else if (
+                    (this.horizontalPosition.startsWith('left') && this.horizontalPosition.endsWith('middle')) ||
+                    (this.horizontalPosition.startsWith('right') && this.horizontalPosition.endsWith('right')) ||
+                    (this.horizontalPosition.startsWith('middle') && this.horizontalPosition.endsWith('right'))
+                ) {
+                    container = 'right';
+                } else if (
+                    (this.horizontalPosition.startsWith('left') && this.horizontalPosition.endsWith('left')) ||
+                    (this.horizontalPosition.startsWith('right') && this.horizontalPosition.endsWith('middle')) ||
+                    (this.horizontalPosition.startsWith('middle') && this.horizontalPosition.endsWith('left'))
+                ) {
+                    container = 'left';
+                }
+            }
+
+            if (horizontal) {
+                if (
+                    (this.verticalPosition.startsWith('top') && this.verticalPosition.endsWith('middle')) ||
+                    (this.verticalPosition.startsWith('bottom') && this.verticalPosition.endsWith('bottom')) ||
+                    (this.verticalPosition.startsWith('middle') && this.verticalPosition.endsWith('bottom'))
+                ) {
+                    container = 'bottom';
+                } else if (
+                    (this.verticalPosition.startsWith('top') && this.verticalPosition.endsWith('top')) ||
+                    (this.verticalPosition.startsWith('bottom') && this.verticalPosition.endsWith('middle')) ||
+                    (this.verticalPosition.startsWith('middle') && this.verticalPosition.endsWith('top'))
+                ) {
+                    container = 'top';
+                }
+            }
+
             return {
-                'vue-ads-absolute': true,
+                horizontal,
+                vertical,
+                container,
             };
         },
 
-        popoverClasses () {
+        centerArrow () {
+            return this.arrow.container === null;
+        },
+
+        disableArrow () {
+            return this.hideArrow || (!this.arrow.vertical && !this.arrow.horizontal);
+        },
+
+        contextContainerClasses () {
             return {
-                'vue-ads-popover-bottom': false,
-                'vue-ads-popover-top': false,
-                'vue-ads-popover-left': true,
-                'vue-ads-popover-right': false,
+                'vue-ads-flex': !this.disableArrow,
+                [`vue-ads-flex-${this.direction[this.arrow.vertical || this.arrow.horizontal]}`]: !this.disableArrow,
             };
+        },
+
+        arrowContainerClasses () {
+            return {
+                'vue-ads-flex': this.arrow.horizontal && this.centerArrow,
+                'vue-ads-flex-col': this.arrow.horizontal  && this.centerArrow,
+                'vue-ads-justify-center': this.arrow.horizontal  && this.centerArrow,
+                'vue-ads-relative': true,
+            };
+        },
+
+        arrowContainerStyles () {
+            let arrowContainerStyles = {};
+
+            if (this.arrow.container) {
+                arrowContainerStyles[this.arrow.vertical ? 'height' : 'width'] = `${this.borderWidth * 2}px`;
+            }
+
+            return arrowContainerStyles;
+        },
+
+        allArrowClasses () {
+            let arrowSize = this.arrowSize === 1 ? '' : `-${this.arrowSize}`;
+
+            return Object.assign(
+                this.arrowClasses,
+                {
+                    'vue-ads-border-solid': true,
+                    'vue-ads-border-transparent': true,
+                    [`vue-ads-border${arrowSize}`]: true,
+                    'vue-ads-mx-auto': this.arrow.vertical && this.centerArrow,
+                    'vue-ads-absolute': this.arrow.container !== null,
+                }
+            );
+        },
+
+        arrowStyles () {
+            let arrowStyles = {
+                [`border-${this.opposit[this.arrow.vertical || this.arrow.horizontal]}-color`]: this.arrowColor,
+                width: `${this.borderWidth * 2}px`,
+            };
+
+            if (this.arrow.container) {
+                arrowStyles[this.arrow.container] = `${this.borderWidth}px`;
+            }
+
+            return arrowStyles;
+        },
+
+        contextClasses () {
+            return {};
+        },
+
+        contextStyles () {
+            return {};
         },
     },
 };
 </script>
-
-<style scoped>
-.vue-ads-popover-bottom:before {
-    border-top: 0.5rem solid rgba(0,0,0,0.85);
-    border-right: 0.5rem solid transparent;
-    border-left: 0.5rem solid transparent;
-    bottom: -0.5rem;
-    left: 50%;
-    margin-left: -0.5rem;
-}
-
-.vue-ads-popover-top:before {
-    border-bottom: 0.5rem solid rgba(0,0,0,0.85);
-    border-right: 0.5rem solid transparent;
-    border-left: 0.5rem solid transparent;
-    top: 0;
-    left: 50%;
-    margin-left: -0.5rem;
-}
-
-.vue-ads-popover-left:before {
-    border-right: 0.5rem solid rgba(0,0,0,0.85);
-    border-top: 0.5rem solid transparent;
-    border-bottom: 0.5rem solid transparent;
-    left: 0;
-    margin-left: -0.5rem;
-    top: 50%;
-    margin-top: -0.5rem;
-}
-
-.vue-ads-popover-right:before {
-    border-left: 0.5rem solid rgba(0,0,0,0.85);
-    border-top: 0.5rem solid transparent;
-    border-bottom: 0.5rem solid transparent;
-    right: 0;
-    margin-right: -0.5rem;
-    top: 50%;
-    margin-top: -0.5rem;
-}
-</style>
